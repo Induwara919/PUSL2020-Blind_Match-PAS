7428cc867702e6503c7197168f48d25bbf6efb37
Induwara <hhiuheamachandra@students.nsbm.ac.lk>
2026-04-14 14:58:08 +0530

    Created the Initial Project Structure and gitignore file

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PUSL2020_Blind_Match_PAS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
