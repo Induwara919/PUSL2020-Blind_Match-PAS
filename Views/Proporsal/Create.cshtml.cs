f842655e90a3a5d06339d3d55853ccc1eb9dece6
Isumi Nethara <isuminethara57@gmail.com>
2026-04-15 02:39:57 +0530

    Implemented Create View with required project fields

---
+++ Views/Proporsal/Create.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PUSL2020_Blind_Match_PAS.Views.Proporsal
+{
+    public class CreateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
