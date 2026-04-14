115e610bcd8e685d491c7d16afe7be4c47e2e79f
Chethana Madushan <madushandesh95@gmail.com>
2026-04-14 15:44:45 +0530

    Created Project Proposal Model

---
+++ Models/ProjectProposal.cs
@@ -0,0 +1,18 @@
+﻿namespace PUSL2020_Blind_Match_PAS.Models
+{
+    public class ProjectProposal
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Abstract { get; set; } = string.Empty;
+        public string TechStack { get; set; } = string.Empty;
+        public string ResearchArea { get; set; } = string.Empty;
+
+        public string Status { get; set; } = "Pending";
+
+        public bool IsRevealed { get; set; } = false;
+
+        public string StudentId { get; set; } = string.Empty;
+        public string? SupervisorId { get; set; }
+    }
+}
