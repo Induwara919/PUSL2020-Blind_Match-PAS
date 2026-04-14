9df1a8f6aa6323a7c7c3ac70e73f9e59b107eda7
Chethana Madushan <madushandesh95@gmail.com>
2026-04-14 16:23:06 +0530

    Setup EF Core and ApplicationDbContext

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PUSL2020_Blind_Match_PAS.Models;
+
+namespace PUSL2020_Blind_Match_PAS.Data
+{
+    
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProjectProposal> Proposals { get; set; }
+    }
+}
