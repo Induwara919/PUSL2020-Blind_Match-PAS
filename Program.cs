5bea1b60e4423d39cbe139626d95404338d64130
Induwara <hhiuheamachandra@students.nsbm.ac.lk>
2026-04-16 00:17:13 +0530

    Implemented Match Reveal logic in ProjectService.cs

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using PUSL2020_Blind_Match_PAS.Data;
+using PUSL2020_Blind_Match_PAS.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +10,7 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddScoped<ProjectService>();
 var app = builder.Build();
 
 if (!app.Environment.IsDevelopment())
