b39ee5b42a05ae4382a57aa2984e0b2dce8a6585
Induwara <hhiuheamachandra@students.nsbm.ac.lk>
2026-04-16 00:38:26 +0530

    Created and implemented Security rules in SupervisorController.cs

---
+++ Controllers/SupervisorController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PUSL2020_Blind_Match_PAS.Data;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace PUSL2020_Blind_Match_PAS.Controllers
+{
+    public class SupervisorController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public SupervisorController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Dashboard()
+        {
+            var anonymousProposals = await _context.Proposals
+                .Where(p => p.IsIdentityRevealed == false)
+                .ToListAsync();
+
+            return View(anonymousProposals);
+        }
+    }
+}
