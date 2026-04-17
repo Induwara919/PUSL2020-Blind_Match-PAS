35ad304c4f5f74384ea811b9d2dfd88e66722d15
Isumi Nethara <isuminethara57@gmail.com>
2026-04-18 03:13:45 +0530

    Added MyProposals logic for status tracking

--- Controllers/ProposalController.cs
+++ Controllers/ProposalController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using PUSL2020_Blind_Match_PAS.Data;
 using PUSL2020_Blind_Match_PAS.Models;
 using System.Threading.Tasks;
+using System.Linq;
 
 namespace PUSL2020_Blind_Match_PAS.Controllers
 {
@@ -14,30 +16,39 @@ namespace PUSL2020_Blind_Match_PAS.Controllers
             _context = context;
         }
 
-        
+       
         public IActionResult Create()
         {
             return View();
         }
 
-        
+       
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create(ProjectProposal proposal)
         {
             if (ModelState.IsValid)
             {
-               
+                
                 proposal.Status = "Pending";
                 proposal.IsIdentityRevealed = false;
 
                 _context.Add(proposal);
                 await _context.SaveChangesAsync();
 
-               
-                return RedirectToAction("Index", "Home");
+                
+                return RedirectToAction(nameof(MyProposals));
             }
             return View(proposal);
+        }
+
+        
+        public async Task<IActionResult> MyProposals()
+        {
+           
+            var proposals = await _context.Proposals.ToListAsync();
+
+            return View(proposals);
         }
     }
 }
