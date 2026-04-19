200d403ba08dea66b6eafe236f43e7c4a243c488
Chethana Madushan <madushandesh95@gmail.com>
2026-04-19 11:56:51 +0530

    Update ApplicationDbContext.cs for better integration

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using PUSL2020_Blind_Match_PAS.Models;
 
@@ -13,5 +13,14 @@ namespace PUSL2020_Blind_Match_PAS.Data
 
         public DbSet<ProjectProposal> Proposals { get; set; }
         public DbSet<Tag> Tags { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Tag>()
+                .HasIndex(t => t.Name)
+                .IsUnique();
+        }
     }
 }
