4085a2c12ca2c31b42f03ee2ebb11cbc3e038674
Chethana Madushan <madushandesh95@gmail.com>
2026-04-19 12:00:56 +0530

    Update Tag.cs for model improvement

--- Models/Tag.cs
+++ Models/Tag.cs
@@ -6,9 +6,11 @@ namespace PUSL2020_Blind_Match_PAS.Models
     {
         [Key]
         public int Id { get; set; }
-        
+
         [Required]
-        [Display(Name = "Research Area")]
-        public string Name { get; set; } 
+        [StringLength(50)]
+        [Display(Name = "Research Area Name")]
+        [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "Area names should only contain letters.")]
+        public string Name { get; set; }
     }
 }
