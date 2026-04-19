c85db833fd4c8d5a6bfdd7f9f6ff1921fc826bd3
Hiruka-Fernando <contact.fernando.whc@gmail.com>
2026-04-19 10:51:19 +0530

    Add authorization and user management to SupervisorController

--- Controllers/SupervisorController.cs
+++ Controllers/SupervisorController.cs
@@ -1,25 +1,31 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using PUSL2020_Blind_Match_PAS.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using PUSL2020_Blind_Match_PAS.Models;
 using System.Threading.Tasks;
 using System.Linq;
 
 namespace PUSL2020_Blind_Match_PAS.Controllers
 {
+    [Authorize(Roles = "Supervisor")]
     public class SupervisorController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public SupervisorController(ApplicationDbContext context)
+        public SupervisorController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
 
         public async Task<IActionResult> Dashboard(string selectedArea)
         {
             ViewBag.AvailableTags = await _context.Tags.Select(t => t.Name).ToListAsync();
-            var query = _context.Proposals.Where(p => !p.IsIdentityRevealed && p.Status != "Matched");
+
+            var query = _context.Proposals.Where(p => !p.IsIdentityRevealed && p.Status == "Pending");
 
             if (!string.IsNullOrEmpty(selectedArea))
             {
@@ -32,8 +38,10 @@ namespace PUSL2020_Blind_Match_PAS.Controllers
 
         public async Task<IActionResult> MyMatches()
         {
+            var user = await _userManager.GetUserAsync(User);
+
             var matchedProjects = await _context.Proposals
-                .Where(p => p.IsIdentityRevealed && p.Status == "Matched")
+                .Where(p => p.IsIdentityRevealed && p.Status == "Matched" && p.SupervisorId == user.Id)
                 .ToListAsync();
 
             return View(matchedProjects);
