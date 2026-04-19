1a4514d80dda47f623c6d47bac152fb40a977b5b
Chethana Madushan <madushandesh95@gmail.com>
2026-04-19 11:58:52 +0530

    Update ApplicationUser.cs with improved user model

--- Models/ApplicationUser.cs
+++ Models/ApplicationUser.cs
@@ -1,10 +1,18 @@
-﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
 
 namespace PUSL2020_Blind_Match_PAS.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        [Required]
+        [PersonalData]
+        [Display(Name = "Full Name")]
         public string FullName { get; set; }
-        public string? StudentId { get; set; } 
+
+        [PersonalData]
+        [Display(Name = "Student ID")]
+        [RegularExpression(@"^[0-9]{8}$", ErrorMessage = "Student ID must be exactly 8 digits.")]
+        public string? StudentId { get; set; }
     }
 }
